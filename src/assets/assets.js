cd2ae16d4b82fc1eeb04546cbe799e9dc03a3d50
Onder Arslan <onder77@gmail.com>
2024-10-28 15:42:19 +0300

    hero added

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_icon from './profile_icon.png'
 import cart_icon from './cart_icon.png'
 import menu_icon from './menu_icon.png'
 import dropdown_icon from './dropdown_icon.png'
+import hero_img from './hero_img.png'
 
 export const assets = {
     logo,
@@ -12,7 +13,8 @@ export const assets = {
     profile_icon,
     cart_icon,
     menu_icon,
-    dropdown_icon
+    dropdown_icon,
+    hero_img
 }
 
 export const products = [{
